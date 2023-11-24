803c037cbd37c8e4c81014c3b56dba5ef4b1007b
HalfACyc <123563700+HalfACyc@users.noreply.github.com>
2023-11-24 13:48:25 +0000

    Add files via upload
    Function to check whether push button has been pushed, if true then there are while loops in void that run indefinitely.

---
+++ PushButtonTest.ino
@@ -0,0 +1,39 @@
+/*
+Code that 
+*/
+
+int StartButton = 3; // Connect push button to input pin 3
+void setup() {
+ Serial.begin(9600);
+ pinMode(StartButton, INPUT); // declare pushbutton as input
+}
+
+// function to check if button has been pushed
+bool checkButtonPress(){
+ int val = digitalRead(StartButton); // read input value
+ if (val == HIGH) { // check if the input is HIGH
+ return true;
+ }
+ else {
+ return false;
+  }
+}
+
+void loop(){
+  bool buttonpush;
+  bool pushed;
+  //while loop checks whether button has been pushed until it has been pushed, sets 'pushed' to true if pushed
+  while (buttonpush != true) {
+    buttonpush = checkButtonPress();
+    if (buttonpush == true){
+      pushed = true;
+      break;
+    }
+  }
+  // pushed indicates button has been pushed after reset/power on - run main code inside while loop
+  while (pushed == true) {
+    //INSERT REAL CODE HERE - OTHERWISE, USE THE BELOW AS A TEST TO SEE IF FUNCTION IS WORKING
+   Serial.println("Button has been pushed, run main function");
+  }
+  
+}
